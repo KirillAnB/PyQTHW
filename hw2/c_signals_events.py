262f728abff275d458ea067b7c6fb4c79da36cd3
KirillAnb <it-reso@mail.ru>
2023-05-15 17:46:35 +0300

    hw2-c_events-signals

--- hw2/c_signals_events.py
+++ hw2/c_signals_events.py
@@ -21,7 +21,8 @@
 """
 
 
-from PySide6 import QtWidgets
+from PySide6 import QtWidgets, QtCore
+import time
 
 from ui.c_signals_events import Ui_Form
 
@@ -32,6 +33,72 @@ class Window(QtWidgets.QWidget):
         super().__init__(parent)
         self.ui = Ui_Form()
         self.ui.setupUi(self)
+        self.initSignals()
+
+    def getWindowState(self):
+        if self.window().isActiveWindow():
+            return 'Active window'
+        elif self.window().isHidden():
+            return 'Hidden window'
+        elif self.window().isVisible():
+            return 'Visible window'
+        elif self.window().isEnabled():
+            return 'Enabled window'
+    def initSignals(self):
+        self.ui.pushButtonGetData.clicked.connect(self.getCurrentData)
+        self.ui.pushButtonLT.clicked.connect(self.moveToLeftTop)
+        self.ui.pushButtonRT.clicked.connect(self.moveToRightTop)
+        self.ui.pushButtonCenter.clicked.connect(self.moveToCenter)
+        self.ui.pushButtonLB.clicked.connect(self.moveToLeftBottom)
+        self.ui.pushButtonRB.clicked.connect(self.moveToRightBottom)
+        self.ui.pushButtonMoveCoords.clicked.connect(self.goToNewPosition)
+    def getCurrentData(self):
+        windows_count = str(len(QtWidgets.QApplication.screens()))
+        current_window = self.objectName()
+        display_size = self.screen().availableSize()
+        current_screen = self.screen().name()
+        window_size = self.size()
+        window_min_size = self.minimumSize()
+        current_coordinates = (self.window().x(), self.window().y())
+        window_center_coorinates = (self.window().x() + self.size().width()//2, self.window().y() + self.size().height()//2)
+        window_state = self.getWindowState()
+        status = f"""{time.ctime()}\nwindows count is {windows_count},\ncurrent window is {current_window},\ndisplay size is {display_size},\ncurrent screen is {current_screen},\nwindow size is {window_size},\nminimumu size is {window_min_size},\ncurrent coordinates are {current_coordinates},\nwindow center is at {window_center_coorinates},\nwindow state is {window_state},\n"""
+        self.ui.plainTextEdit.setPlainText(status)
+
+    def moveToCenter(self):
+        window_size = self.screen().availableSize()
+        x = (window_size.width()-self.size().width()) // 2
+        y = (window_size.height() - self.size().height()) // 2
+        self.move(x, y)
+
+    def moveToLeftTop(self):
+        self.move(0, 0)
+
+    def moveToRightTop(self):
+        screen = self.screen().availableGeometry()
+        self.move(screen.width()-self.size().width(), 0)
+
+    def moveToRightBottom(self):
+        screen = self.screen().availableGeometry()
+        self.move(screen.width()-self.size().width(), screen.height()-self.size().height())
+
+    def moveToLeftBottom(self):
+        screen = self.screen().availableGeometry()
+        self.move(0, screen.height()-self.size().height())
+
+
+    def goToNewPosition(self):
+        old_x, old_y = self.window().x(), self.window().y()
+        new_x, new_y = self.ui.spinBoxX.value(), self.ui.spinBoxY.value()
+        self.move(new_x, new_y)
+
+    # def event(self, event: QtCore.QEvent) -> bool:
+    #     old_size = self.size()
+    #     old_position = (self.window().x(), self.window().y())
+    #     if event.type() == QtCore.QEvent.Type.Resize:
+    #         print(f'{time.ctime()} resized\nnew size is {self.size()}')
+    #     if event.type() == QtCore.QEvent.Type.Move:
+    #         print(f'{time.ctime()} new position is {self.window().x(), self.window().y()}')
 
 
 if __name__ == "__main__":
