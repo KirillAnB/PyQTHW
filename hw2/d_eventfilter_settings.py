3d7eba04afc4b277108186a4c86dcbeb1ca8e9e0
KirillAnb <it-reso@mail.ru>
2023-05-16 17:04:07 +0300

    доделал d_eventfilter

--- hw2/d_eventfilter_settings.py
+++ hw2/d_eventfilter_settings.py
@@ -18,7 +18,7 @@
    в него соответствующие значения
 """
 
-from PySide6 import QtWidgets
+from PySide6 import QtWidgets, QtCore, QtGui
 from ui.d_eventfilter_settings import Ui_Form
 
 
@@ -28,6 +28,57 @@ class Window(QtWidgets.QWidget):
         super().__init__(parent)
         self.ui = Ui_Form()
         self.ui.setupUi(self)
+        self.settings = QtCore.QSettings("d_event_filter_settings")
+        self.initUi()
+        self.initSignals()
+        self.initSettings()
+
+    def initUi(self):
+        self.ui.comboBox.addItem('hex')
+        self.ui.comboBox.addItem('okt')
+        self.ui.comboBox.addItem('bin')
+        self.ui.comboBox.addItem('dec')
+        self.ui.comboBox.setCurrentIndex(0)
+    def initSignals(self):
+        self.ui.comboBox.currentTextChanged.connect(self.comboBoxChanged)
+        self.ui.horizontalSlider.valueChanged.connect(self.hSliderMoved)
+        self.ui.dial.valueChanged.connect(self.dialTurned)
+
+    def initSettings(self):
+        self.settings = QtCore.QSettings("d_event_filter_kiriand")
+        self.ui.lcdNumber.display(self.settings.value('LCD', '0'))
+        self.ui.comboBox.setCurrentText(self.settings.value('ComboBox', 'hex'))
+
+    def comboBoxChanged(self):
+        if self.ui.comboBox.currentText() == 'hex':
+            self.ui.lcdNumber.setHexMode()
+        if self.ui.comboBox.currentText() == 'okt':
+            self.ui.lcdNumber.setOctMode()
+        if self.ui.comboBox.currentText() == 'bin':
+            self.ui.lcdNumber.setBinMode()
+        if self.ui.comboBox.currentText() == 'dec':
+            self.ui.lcdNumber.setDecMode()
+
+
+    def hSliderMoved(self):
+        self.ui.dial.setValue(self.ui.horizontalSlider.value())
+        self.ui.lcdNumber.display(self.ui.horizontalSlider.value())
+
+    def dialTurned(self):
+        self.ui.horizontalSlider.setValue(self.ui.dial.value())
+        self.ui.lcdNumber.display(self.ui.dial.value())
+
+    def keyPressEvent(self, event: QtGui.QKeyEvent) -> None:
+        if event.key() == 61 or event.key() == 43:
+            self.ui.dial.setValue(self.ui.dial.value() + 1)
+        if event.key() == 45:
+            self.ui.dial.setValue(self.ui.dial.value() - 1)
+
+    def closeEvent(self, event: QtGui.QCloseEvent) -> None:
+        self.settings = QtCore.QSettings("d_event_filter_kiriand")
+        self.settings.setValue('LCD', self.ui.lcdNumber.value())
+        self.settings.setValue('ComboBox', self.ui.comboBox.currentText())
+
 
 
 
